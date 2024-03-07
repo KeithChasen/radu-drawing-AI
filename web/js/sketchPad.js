7a6df5e85e1a25bb5120d79b5e9d2e4e482b5414
keithchasen <keithchasen89@gmail.com>
2024-03-07 16:40:51 +0100

    drawing single dot path with mouse

---
+++ web/js/sketchPad.js
@@ -0,0 +1,54 @@
+class SketchPad {
+    constructor(container, size=400) {
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = size;
+        this.canvas.height = size;
+        this.canvas.style =`
+            background-color: white;
+            box-shadow: 0px 0px 10px 2px black
+        `
+        container.appendChild(this.canvas);
+
+        this.ctx = this.canvas.getContext('2d');
+
+        this.path = [];
+        this.isDrawing = false;
+
+        this.#addEventListener();
+    }
+
+    #addEventListener() {
+        this.canvas.onmousedown = e => {
+            const mouse = this.#getMouse(e)
+            this.path = [mouse];
+            this.isDrawing = true;
+        }
+
+        this.canvas.onmousemove = e => {
+            if (this.isDrawing) {
+                const mouse = this.#getMouse(e)
+                this.path.push(mouse);
+                this.#redraw();
+            }   
+        }
+
+        this.canvas.onmouseup = () => {
+            this.isDrawing = false;
+        }
+    }
+
+    #redraw() {
+        this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+        draw.path(this.ctx, this.path);
+    }
+
+    #getMouse = e => {
+        const rect = this.canvas.getBoundingClientRect();
+
+        // relative coordinates of mouse inside the canvas
+        return [
+            Math.round(e.clientX - rect.left),
+            Math.round(e.clientY - rect.top),
+        ];
+    }
+}
